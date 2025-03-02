a2301329403475aa58518999a50f5b94102ab0dc
Tanushka18804 <tanushkachaudhary46@gmail.com>
2025-03-02 17:51:18 +0530

    app.js

--- app.js
+++ app.js
@@ -5,22 +5,18 @@ import { connectDB } from "./DB/Database.js";
 import transactionRoutes from "./Routers/Transactions.js";
 import userRoutes from "./Routers/userRouter.js";
 
-//app is express server
 const app = express();
 const port = 4000;
 
-//database connection triggered
 connectDB();
-//we are registering cors - Cross Origin Resource Sharing - This allow our server to respond to our frontend requests
+
 app.use(cors());
 
-// Middleware - is a logic which executes before the backend
-app.use(express.json());//{key:value}
-app.use(bodyParser.urlencoded({ extended: false }));//name=hmg
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-// Adding 2 routes
-app.use("/api/v1", transactionRoutes);//all end points related to credit/debit transactions
-app.use("/api/auth", userRoutes);//this all end points related to users -> login, signup
+app.use("/api/v1", transactionRoutes);
+app.use("/api/auth", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("FinManager Server is working");
